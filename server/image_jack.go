65a40abe879ce099b47f667e8b3da7626e1182f5
flyotlin <flyotlin@gmail.com>
2022-09-17 12:46:14 +0800

    feat: new image processing methods
    - resizeByFactor
    - crop
    - testing for these methods

--- server/image_jack.go
+++ server/image_jack.go
@@ -29,3 +29,62 @@ func (j *ImageJack) Resize(width int, height int) *gocv.Mat {
 	gocv.Resize(*j.cvMat, &resizedImg, image.Point{width, height}, 0, 0, gocv.InterpolationDefault)
 	return &resizedImg
 }
+
+func (j *ImageJack) ResizeByFactor(fw float64, fh float64) *gocv.Mat {
+	resizedImg := gocv.NewMat()
+	gocv.Resize(*j.cvMat, &resizedImg, image.Point{}, fw, fh, gocv.InterpolationDefault)
+	return &resizedImg
+}
+
+func (j *ImageJack) Crop(head image.Point, tail image.Point) *gocv.Mat {
+	if !isValidRect(head, tail) {
+		log.Errorf("invalid crop rectangle: [%v, %v]", head, tail)
+		return nil
+	}
+
+	imgHead := image.Point{0, 0}
+	imgTail := image.Point{j.cvMat.Size()[1], j.cvMat.Size()[0]}
+	head = clipAbove(head, imgHead)
+	tail = clipLower(tail, imgTail)
+
+	cropRect := image.Rect(head.X, head.Y, tail.X, tail.Y)
+	croppedImg := j.cvMat.Region(cropRect)
+	return &croppedImg
+}
+
+func isValidRect(head image.Point, tail image.Point) bool {
+	if tail.X < head.X {
+		return false
+	}
+	if tail.Y < head.Y {
+		return false
+	}
+	return true
+}
+
+func clipAbove(p image.Point, clipP image.Point) image.Point {
+	return clipPoint(p, clipP, true)
+}
+
+func clipLower(p image.Point, clipP image.Point) image.Point {
+	return clipPoint(p, clipP, false)
+}
+
+func clipPoint(p image.Point, clipP image.Point, isAbove bool) image.Point {
+	if isAbove {
+		if p.X < clipP.X {
+			p.X = clipP.X
+		}
+		if p.Y < clipP.Y {
+			p.Y = clipP.Y
+		}
+	} else {
+		if p.X > clipP.X {
+			p.X = clipP.X
+		}
+		if p.Y > clipP.Y {
+			p.Y = clipP.Y
+		}
+	}
+	return p
+}
--- server/image_jack_test.go
+++ server/image_jack_test.go
@@ -1,9 +1,88 @@
 package server
 
 import (
+	"image"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gocv.io/x/gocv"
 )
 
-func TestResize(t *testing.T) {
+func newFakeImage(width int, height int) *gocv.Mat {
+	mat := gocv.NewMatWithSize(height, width, gocv.MatTypeCV16S)
+	return &mat
+}
 
+func TestResize(t *testing.T) {
+	w, h := 1200, 800
+	resizedW, resizedH := 500, 300
+
+	mat := newFakeImage(w, h)
+	jack := ImageJack{cvMat: mat}
+
+	resizedMat := jack.Resize(resizedW, resizedH)
+
+	assert.Equal(t, resizedW, resizedMat.Size()[1])
+	assert.Equal(t, resizedH, resizedMat.Size()[0])
+}
+
+func TestResizeByFactor(t *testing.T) {
+	w, h := 1200, 800
+	fw, fh := 0.6, 0.4
+
+	mat := newFakeImage(w, h)
+	jack := ImageJack{cvMat: mat}
+
+	resizedMat := jack.ResizeByFactor(fw, fh)
+
+	assert.Equal(t, float64(w)*fw, float64(resizedMat.Size()[1]))
+	assert.Equal(t, float64(h)*fh, float64(resizedMat.Size()[0]))
+}
+
+func TestCropReturnCorrectSize(t *testing.T) {
+	w, h := 640, 480
+	head := image.Point{50, 60}
+	tail := image.Point{100, 120}
+
+	mat := newFakeImage(w, h)
+	jack := ImageJack{cvMat: mat}
+
+	croppedImg := jack.Crop(head, tail)
+	assert.Equal(t, tail.X-head.X, croppedImg.Size()[1])
+	assert.Equal(t, tail.Y-head.Y, croppedImg.Size()[0])
+}
+
+// 搞錯 w,h -> row,col -> .Size() 0 1，透過 testing + debugger 快速排除
+func TestCropReturnCorrectSizeWhenOutOfBound(t *testing.T) {
+	w, h := 640, 480
+	heads := []image.Point{
+		{-100, 0},
+		{4, -100},
+		{-20, -30},
+	}
+	tails := []image.Point{
+		{50, h + 20},
+		{w + 10, 50},
+		{w + 15, h + 30},
+	}
+	expected := []image.Point{
+		{50, 480},  // 0,0->50,480
+		{636, 50},  // 4,0->640,50
+		{640, 480}, // 0,0->640,480
+	}
+
+	mat := newFakeImage(w, h)
+	jack := ImageJack{cvMat: mat}
+
+	if len(heads) != len(tails) {
+		t.Log("the number of elements in heads and tails must be the same")
+		t.FailNow()
+	}
+	lens := len(heads)
+
+	for i := 0; i < lens; i++ {
+		croppedImg := jack.Crop(heads[i], tails[i])
+		assert.Equal(t, expected[i].X, croppedImg.Size()[1])
+		assert.Equal(t, expected[i].Y, croppedImg.Size()[0])
+	}
 }
